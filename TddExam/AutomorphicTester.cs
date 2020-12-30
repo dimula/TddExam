b63f23fb56e6fb67baccd3e61f3d9d89528c2dd2
Breiman, Dmitri <dmitri.breiman@worksafebc.com>
2020-12-30 12:46:53 -0800

    Add project files.

---
+++ TddExam/AutomorphicTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TddExam
+{
+    public class AutomorphicTester 
+    {
+        public bool IsAutomorphicTester(int number)
+        {
+            if (number < 0)
+                throw new ArgumentOutOfRangeException($"Input must be positiv number. Input: {number}");
+
+            var res = checked (number * number);
+            return res.ToString().Substring(res.ToString().Length - number.ToString().Length) == number.ToString();
+        }
+    }
+}
---
+++ TddExamTests/AutomorphicTesterTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddExam;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TddExam.Tests
+{
+    [TestClass()]
+    public class AutomorphicTesterTests
+    {
+        AutomorphicTester _automorphicTester;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _automorphicTester = new AutomorphicTester();
+        }
+        
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _automorphicTester = null;
+        }
+
+        [TestMethod()]
+        public void IsAutomorphicTester_AutomorphicNumbersTest()
+        {
+            var testArr = new int[] { 0, 1, 5, 6, 76, 376 };
+            foreach(var num in testArr)
+            {
+                var res = _automorphicTester.IsAutomorphicTester(num);
+                Assert.IsTrue(res);
+            }
+        }
+
+        [TestMethod()]
+        public void IsAutomorphicTester_NonAutomorphicNumbersTest()
+        {
+            var testArr = new int[] { 2,3,777,895,120};
+            foreach (var num in testArr)
+            {
+                var res = _automorphicTester.IsAutomorphicTester(num);
+                Assert.IsFalse(res);
+            }
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void IsAutomorphicTester_NegativeInputThrowsArgumentOutOfRangeExceptionTest()
+        {
+            _automorphicTester.IsAutomorphicTester(-5);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(OverflowException))]
+        public void IsAutomorphicTester_OverflowExceptionTest()
+        {
+            _automorphicTester.IsAutomorphicTester(int.MaxValue);
+        }
+    }
+}
